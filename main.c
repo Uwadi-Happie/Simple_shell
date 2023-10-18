899f0e1ff14beb15cba2f03e406bf1505cc84bbf
vixent <alagovincent03@gmail.com>
2023-10-18 13:05:13 -0700

    done

---
+++ main.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+
+/**
+ * main - entry point
+ * @ac: argument count
+ * @av: argument
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	info_t data[] = { INFO_INIT };
+	int filedes = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (filedes)
+		: "r" (filedes));
+
+	if (ac == 2)
+	{
+		filedes = open(av[1], O_RDONLY);
+		if (filedes == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		data->readfd = filedes;
+	}
+	populate_env_list(data);
+	read_history(data);
+	hsh(data, av);
+	return (EXIT_SUCCESS);
+}
+
